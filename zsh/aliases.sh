24a74bfdc5b54473ed0b08fe259de9f3b2236d29
karin0 <karin0@gmx.com>
2021-02-20 22:48:46 +0800

    qwq

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -4,7 +4,7 @@ alias mv='mv -i'
 alias lt='ls -lAhtr'
 alias rm='trash-put -v'
 
-alias ncm='sudo node ~/clones/UnblockNeteaseMusic/app.js -f 59.111.181.38 -p 80:443 &; netease-cloud-music --ignore-certificate-errors'
+# alias ncm='sudo node ~/clones/UnblockNeteaseMusic/app.js -f 59.111.181.38 -p 80:443 &; netease-cloud-music --ignore-certificate-errors'
 
 alias px='HTTP_PROXY=http://127.0.0.1:10808 HTTPS_PROXY=http://127.0.0.1:10808 ALL_PROXY=http://127.0.0.1:10808'
 
@@ -13,7 +13,19 @@ mkcd () {
     cd -P -- "$1"
 }
 
-gacp() {
+gac() {
     LOG="$*"
-    git diff --name-only && read -n 1 && git add . && git commit -m $LOG && read -n 1 && proxychains4 git push
+    git diff --name-only && \
+    read -n 1 && \
+    git add . && \
+    git commit -m "$LOG" && \
+    read -n 1
+}
+
+gacp() {
+    gac "$*" && git push
+}
+
+pgacp() {
+    gac "$*" && proxychains4 git push
 }
