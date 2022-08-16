6aa08ef72ca67efa0650b4e6bfba47224debf492
karin0 <karin0@gmx.com>
2022-08-17 01:53:12 +0800

    add bin

--- zsh/common.sh
+++ zsh/common.sh
@@ -8,4 +8,4 @@ elif vi --version >/dev/null 2>&1; then
   export EDITOR="vi"
 fi
 
-export PATH=$HOME/lark/bin:$HOME/bin:$HOME/.local/bin:$HOME/.yarn/bin:$PATH
+export PATH=$HOME/dotfiles/bin:$HOME/lark/bin:$HOME/bin:$HOME/.local/bin:$HOME/.yarn/bin:$PATH
