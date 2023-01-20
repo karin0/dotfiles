6c07123960daac9c40d130079cf363d356ed91fd
karin0 <karin0@gmx.com>
2023-01-20 21:27:38 +0800

    update git

--- git/import.sh
+++ git/import.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 set -e
 
-# https://stackoverflow.com/questions/55249773
-cat gitconfig |
-while read line; do
-  git config --global \
-    "`echo $line | sed 's/=.\+//'`" \
-    "`echo $line | sed 's/^.\+=//'`"
-done
+while read -r line; do
+  git config --global "${line/%=?*/}" "${line/#?*=/}"
+done < gitconfig
