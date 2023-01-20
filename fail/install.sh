4dda46021ebf3de630d4d9380c0a11a03d385409
karin0 <karin0@gmx.com>
2023-01-20 21:46:38 +0800

    fix sys-fail

--- fail/install.sh
+++ fail/install.sh
@@ -8,9 +8,11 @@ add fail.conf $dir/fail.conf
 systemctl --user daemon-reload
 
 dir=/etc/systemd/system/service.d
-sudo mkdir -p $dir
+sudo mkdir -p $dir /opt/dotfiles
+sudo cp -v post-stop ~/bin/kpush /opt/dotfiles/
 
 ADD_SUDO=sudo
 ADD_LN='cp -nv'
 add sys-fail.conf $dir/sys-fail.conf
+
 sudo systemctl daemon-reload
