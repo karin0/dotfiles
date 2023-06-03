c018f86424a847908e129ba23fb68ad1dd77ab9e
karin0 <karin0@gmx.com>
2023-06-04 01:16:15 +0800

    fix installing from home

--- install.sh
+++ install.sh
@@ -5,8 +5,9 @@ cd "$HERE"
 . ./utils.bash
 
 in_path zsh && add zsh/zshrc.zsh ~/.zshrc
-in_path vim && add vim/vimrc ~/.vimrc
-in_path tmux && add tmux/tmux.conf ~/.tmux.conf
+in_path vim && add home/.vimrc ~/.vimrc
+in_path tmux && add home/.tmux.conf ~/.tmux.conf
+in_path termux-wake-lock && add home/.termux ~/.termux
 
 if [ -f ~/dotsecrets/install.sh ]; then
   HERE=~/dotsecrets
